a670950eac1625d815cd259338001255b4b26dfd
asmith <mrpresidentsmith@gmail.com>
2019-04-26 17:37:15 -0700

    Initial Commit.  Created project.  Implemented animated splash screen.

---
+++ Maze of Death/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex + 1);
+    }
+
+    public void LoadNextLevelAfterDelay(float delay)
+    {
+        Invoke("LoadNextLevel", delay);
+    }
+}
