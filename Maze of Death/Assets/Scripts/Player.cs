fb2342a2287e3491ede523587a303691d148667f
asmith <mrpresidentsmith@gmail.com>
2019-04-28 18:36:21 -0700

    Created a scene called Anthony for testing.  added a teal square and a yellow circle.  started experimenting with player movement.  Don't think physics will be the way to go, but need to do research.

---
+++ Maze of Death/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float speedModifier = 5f;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            Vector2 newPosition = new Vector2(Input.GetAxis("Horizontal")* speedModifier * Time.deltaTime, 0f);
+            rb2d.AddForce(newPosition, ForceMode2D.Impulse);
+        }
+    }
+}
