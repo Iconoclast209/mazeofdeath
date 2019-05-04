dd94ad9315658fcf03620c1fd92103c671a6fb1d
Iconoclast209 <mrpresidentsmith@gmail.com>
2019-05-04 08:42:27 -0700

    Using RigidBody2D.MovePosition() to move the kinematic rigidbody for the player.  This does not allow for collision with the tilemap, in the same way that dynamic colliders do.

--- Maze of Death/Assets/Scripts/Player.cs
+++ Maze of Death/Assets/Scripts/Player.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float speedModifier = 5f;
+    [SerializeField]
+    float speedModifier = 4f;
     Rigidbody2D rb2d;
+    Vector2 currentPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        currentPosition = new Vector2(transform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(Input.GetAxis("Horizontal") != 0)
+        currentPosition = new Vector2(transform.position.x, transform.position.y);
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
         {
-            Vector2 newPosition = new Vector2(Input.GetAxis("Horizontal")* speedModifier * Time.deltaTime, 0f);
-            rb2d.AddForce(newPosition, ForceMode2D.Impulse);
+            //Determine where the player is going to move to
+            Vector2 newPosition = new Vector2(currentPosition.x + (Input.GetAxis("Horizontal") * speedModifier * Time.deltaTime),
+                currentPosition.y + (Input.GetAxis("Vertical") * speedModifier * Time.deltaTime));
+            //Then move the player
+            rb2d.MovePosition(newPosition);
+            Debug.Log("Player has moved.");
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player has entered the tilemap collider.");
+        //If the player enters the tilemap collider, then nudge position back to former position.
+        rb2d.MovePosition(currentPosition);
+    }
+
 }
